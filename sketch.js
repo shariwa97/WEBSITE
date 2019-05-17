53c4d0da4e27c3bfcaf5572790d401de3eddfe6b
shariwa97 <47484401+shariwa97@users.noreply.github.com>
2019-05-17 11:35:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,12 +10,8 @@ function setup() {
 function draw() {
   strokeWeight(random(0.5,3));
   noStroke();
-  fill(8,25,156,random(0,4))
+  fill(255,207,210,random(1,20))
   rect(mouseX,mouseY,random(1,900),random(1,40),20);
-}
-
-function mousePressed(){
-	reset();
 }
 
 function windowResized() {
